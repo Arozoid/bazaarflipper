8d56b5abacd25f6e45173318ad02cefc4210b146
arozoid <83493465+Arozoid@users.noreply.github.com>
2024-10-06 14:19:36 +0000

    added comments and moved pieces of code to better places

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Replace underscores with spaces, format the words into the title format and returns the final text
 function formatTitle(str) {
   str = str.replace(/_/g, ' ');
 
@@ -6,6 +7,7 @@ function formatTitle(str) {
   });
 }
 
+// Get JSON from a URL
 async function getData(url) {
     try {
       const response = await fetch(url);
@@ -19,50 +21,77 @@ async function getData(url) {
       console.error(error.message);
     }
   }
-  
-function addOrders(bzInfo) {
-  
-  getData("https://api.slothpixel.me/api/skyblock/items")
-    .then((data) => {
-      let itemInfo = data;
-      
-      let dataKeys = Object.keys(bzInfo);
-  		document.getElementById("bz").innerHTML = "";
-      
-      for (let i = 0; i < dataKeys.length; i++) {
-        let element = document.createElement("p");
-        let product = bzInfo[dataKeys[i]];
-        
-        let product_item;
-        
-        if (dataKeys[i] == "BAZAAR_COOKIE") {
-          product_item = {'name': "Booster Cookie"}
-        } else if (itemInfo[dataKeys[i]]) {
-          product_item = itemInfo[dataKeys[i]]
-        } else {
-        	product_item = {'name': formatTitle(dataKeys[i])}
-        }
-        
-        if (product && product.hasOwnProperty('quick_status')) {
-          element.textContent = `${product_item['name']}: ${JSON.stringify(product['quick_status'])}`;
-        } else {
-          element.textContent = "Invalid product data";
-        }
 
-        document.getElementById("bz").appendChild(element);
+// Add orders to website
+function addOrders(bzInfo, itemInfo) {
+    // Put item list in one variable
+    let itemInfo = data;
+      
+    // Obtains all keys in the dictionary
+    let dataKeys = Object.keys(bzInfo);
+  
+    // Removes all content from the "bz" divider
+    document.getElementById("bz").innerHTML = "";
+        
+    // For loop for all bazaar items
+    for (let i = 0; i < dataKeys.length; i++) {
+      // Create product element
+      let element = document.createElement("p");
+  
+      // Get product info
+      let product = bzInfo[dataKeys[i]];
+          
+      // Create variable for the products corresponding data in the item list
+      let product_item;
+          
+      // Check for bazaar specific items
+      if (dataKeys[i] == "BAZAAR_COOKIE") {
+        // If it is a "bazaar cookie", add the name as "Booster Cookie"
+        product_item = {'name': "Booster Cookie"}
+      } else if (itemInfo[dataKeys[i]]) {
+        // If the item does not exist in the item catalog, format the ID and make it the name
+        product_item = itemInfo[dataKeys[i]]
+      } else {
+        // If the item is in the item catalog
+        product_item = {'name': formatTitle(dataKeys[i])}
       }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+          
+      // Double check if API is working properly before reading item data
+      if (product && product.hasOwnProperty('quick_status')) {
+        // If all goes normal
+        element.textContent = `${product_item['name']}: ${JSON.stringify(product['quick_status'])}`;
+      } else {
+        // If something is wrong with the API
+        element.textContent = "Unable to read item data";
+      }
+          
+      // Append product data to the "bz" divider
+      document.getElementById("bz").appendChild(element);
+  }
 }
 
 function updateWebsite() {
   getData("https://api.slothpixel.me/api/skyblock/bazaar")
+    // Code if the request succeeds to get Bazaar data
     .then((data) => {
+      // Store Bazaar data
       let bazaarInfo = data;
-      addOrders(bazaarInfo);
+
+      // Get list of all items in Skyblock
+      getData("https://api.slothpixel.me/api/skyblock/items")
+        // Code if the request succeeds to get the item catalog
+        .then((data) => {
+          let itemInfo = data;
+          addOrders(bazaarInfo, itemInfo);
+        })
+
+        // Code if the request fails to get the item catalog
+        .catch((error) => {
+          console.error(error);
+        });
     })
+
+    // Code if the request fails to get the Bazaar data
     .catch((error) => {
       console.error(error);
     });
