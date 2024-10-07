7cc7dd539a4f849d8c46d0b93efbfe7329eba5f2
arozoid <83493465+Arozoid@users.noreply.github.com>
2024-10-07 14:45:42 +0000

    code rewrite (w.i.p.)

--- main.js
+++ main.js
@@ -1,103 +1,117 @@
+// Calculates coins per hour.
+function getCoinsPerHour(buyPrice, sellPrice, supply, demand) {
+  let hourlyTransactions = Math.min(supply, demand) / 168
+  let profitMargin = buyPrice - sellPrice
+  let totalProfit = profitMargin * hourlyTransactions
+  return totalProfit
+}
+
+// Formats numbers. Example: 7764941 => 7,764,941
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
 // Replace underscores with spaces, format the words into the title format and returns the final text
 function formatTitle(str) {
-  str = str.replace(/_/g, ' ');
+  str = str.replace(/_/g, " ")
 
-  return str.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  })
 }
 
 // Get JSON from a URL
 async function getData(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      return json;
-    } catch (error) {
-      console.error(error.message);
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
+    }
+
+    const json = await response.json()
+    return json
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+// Add orders to website
+function addOrders(bzInfo, itemInfo) {
+  // Create product variables
+  let product = {}
+
+  for (const id in bzInfo) {
+    // Store all important product data
+    product[id] = {
+      // Buy and sell prices
+      buyPrice: bzInfo[id]["quick_status"]["buyPrice"],
+      sellPrice: bzInfo[id]["quick_status"]["sellPrice"],
+
+      // Supply and demand
+      supply: bzInfo[id]["quick_status"]["sellMovingWeek"],
+      demand: bzInfo[id]["quick_status"]["buyMovingWeek"],
+
+      // Coins per hour
+      coinsPerHour: getCoinsPerHour(
+        bzInfo[id]["quick_status"]["buyPrice"],
+        bzInfo[id]["quick_status"]["sellPrice"],
+        bzInfo[id]["quick_status"]["sellMovingWeek"],
+        bzInfo[id]["quick_status"]["buyMovingWeek"],
+      ),
+
+      // Item info
+      item: itemInfo.find((dict) => dict["id"] === id)
+        ? itemInfo.find((dict) => dict["id"] === id)["id"]
+        : formatTitle(id),
     }
   }
 
-// Add orders to website
-function addOrders(bzInfo, itemInfo) {     
-    // Obtains all keys in the dictionary
-    let dataKeys = Object.keys(bzInfo);
-  
-    // Removes all content from the "bz" divider
-    document.getElementById("bz").innerHTML = "";
-        
-    // For loop for all bazaar items
-    for (let i = 0; i < dataKeys.length; i++) {
-      // Create product element
-      let divider = document.createElement("div");
-  
-      // Get product info
-      let product = bzInfo[dataKeys[i]];
-          
-      // Create variable for the products corresponding data in the item list
-      let productItem;
-      
-      try {
-      	productItem = itemInfo.find(dict => dict['id'] === dataKeys[i]);
-      } catch(err) {}
-      
-      // Check for bazaar specific items
-      if (dataKeys[i] == "BAZAAR_COOKIE") {
-        // If it is a "bazaar cookie", add the name as "Booster Cookie"
-        productItem = {'name': "Booster Cookie"};
-      } else if (productItem) {
-        // If the item is in the item catalog
-        productItem = productItem;
-      } else {
-        // If the item does not exist in the item catalog, format the ID and make it the name
-        productItem = {'name': formatTitle(dataKeys[i])};
-      }
-          
-      // Double check if API is working properly before reading item data
-      if (product && product.hasOwnProperty('quick_status')) {
-        // If all goes normal
-        
-        // Product name
-        productName = document.createElement("p");
-        productName.textContent = `${productItem['name']}: `;
-        
-        // Product buy price
-        productBuyPrice = document.createElement("p")
-        productBuyPrice.textContent = `Buy Price: ${product['quick_status']['buyPrice'].toFixed(1)}`
-        
-        // Product sell price
-        productSellPrice = document.createElement("p")
-        productSellPrice.textContent = `Sell Price: ${product['quick_status']['sellPrice'].toFixed(1)}`
-        
-        // Product supply
-        productSupply = document.createElement("p")
-        productSupply.textContent = `Supply: ${product['quick_status']['sellMovingWeek'].toFixed(1)}`
-        
-        // Product demand
-        productDemand = document.createElement("p")
-        productDemand.textContent = `Demand: ${product['quick_status']['buyMovingWeek'].toFixed(1)}`
-        
-        productCoinsPerHour = document.createElement("p")
-        productCoinsPerHour.textContent = `Coins per hour (no tax): ${Math.sqrt((product['quick_status']['buyMovingWeek'] / 7 / 24) * (product['quick_status']['sellMovingWeek'] / 7 / 24)) * (product['quick_status']['buyPrice'] - product['quick_status']['sellPrice'])}`
-        
-      } else {
-        // If something is wrong with the API
-        productName = document.createElement("p");
-        productName.textContent = "Unable to read item data";
-      }
-      divider.appendChild(productName)
-      divider.appendChild(productBuyPrice)
-      divider.appendChild(productSellPrice)
-      divider.appendChild(productSupply)
-      divider.appendChild(productDemand)
-      divider.appendChild(productCoinsPerHour)
-      
-      // Append product data to the "bz" divider
-      document.getElementById("bz").appendChild(divider);
+  // Order the products dictionary
+  let productOrder = Object.entries(product)
+
+  productOrder = productOrder.sort(
+    (a, b) => b[1].coinsPerHour - a[1].coinsPerHour,
+  )
+  product = Object.fromEntries(productOrder)
+
+  // Update website content
+  document.getElementById("bz").innerHTML = ""
+
+  for (const id in product) {
+    // Create a bazaar info wrapper
+    let bzDiv = document.createElement("div")
+    bzDiv.id = "bzInfo-wrapper"
+
+    // Name
+    let name = document.createElement("p")
+    name.textContent = `${product[id]["item"]["name"]}:`
+
+    // Buy and sell price
+    let buyPrice = document.createElement("p")
+    buyPrice.textContent = `Buy Price: ${product[id]["buyPrice"]}`
+
+    let sellPrice = document.createElement("p")
+    sellPrice.textContent = `Sell Price: ${product[id]["sellPrice"]}`
+
+    // Supply and demand
+    let supply = document.createElement("p")
+    supply.textContent = `Supply: ${product[id]["supply"]}`
+
+    let demand = document.createElement("p")
+    demand.textContent = `Demand: ${product[id]["demand"]}`
+
+    // Coins per hour
+    let coinsPerHour = document.createElement("p")
+    coinsPerHour.textContent = `Coins per hour (no tax): ${product[id]["coinsPerHour"]}`
+
+    bzDiv.appendChild(name)
+    bzDiv.appendChild(buyPrice)
+    bzDiv.appendChild(sellPrice)
+    bzDiv.appendChild(supply)
+    bzDiv.appendChild(demand)
+    bzDiv.appendChild(coinsPerHour)
+
+    document.getElementById("bz").appendChild(bzDiv)
   }
 }
 
@@ -106,29 +120,29 @@ function updateWebsite() {
     // Code if the request succeeds to get Bazaar data
     .then((data) => {
       // Store Bazaar data
-      let bazaarInfo = data['products'];
+      let bazaarInfo = data["products"]
 
       // Get list of all items in Skyblock
       getData("https://api.hypixel.net/v2/resources/skyblock/items")
         // Code if the request succeeds to get the item catalog
         .then((data) => {
-        	// Put item catalog into one variable
-          let itemData = data['items'];
-          
+          // Put item catalog into one variable
+          let itemData = data["items"]
+
           // Add orders to the website
-          addOrders(bazaarInfo, itemData);
+          addOrders(bazaarInfo, itemData)
         })
 
         // Code if the request fails to get the item catalog
         .catch((error) => {
-          console.error(error);
-        });
+          console.error(error)
+        })
     })
 
     // Code if the request fails to get the Bazaar data
     .catch((error) => {
-      console.error(error);
-    });
+      console.error(error)
+    })
 }
 
 // Update the website every 5 seconds
